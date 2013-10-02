f459f1c23858939cb6ee4c302c32e0d60529c582
Michael Kelley <mike@etszone.com>
2013-10-02 10:33:56 -0500

    Program now tests for fps.
    Removed some unnecessary comments and vestigial code.

--- src/test.cpp
+++ src/test.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
+#include <time.h>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+
+double testFps(VideoCapture& cap) {
+    if (!cap.isOpened()) {
+        return -1;
+    }
+
+    time_t start, end;
+    double fps;
+    double sec;
+    int counter = 0;
+    time(&start);
+    Mat image;
+    while(counter < 120) {
+        cap >> image;
+        time(&end);
+        ++counter;
+    }
+    sec = difftime(end, start);
+    fps = counter / sec;
+    return fps;
+}
 
 int main(int, char**) {
     VideoCapture cap(0);
     if (cap.isOpened()) {
         std::cout << "Opened camera.\n";
     } else {
-        std::cout << "Couldn't find.\n";
+        std::cout << "Couldn't open camera.\n";
         return -1;
     }
+
+    double fps = testFps(cap);
+    std::cout << "FPS: "  << fps << std::endl;
+
     Size inputSize = Size((int)cap.get(CV_CAP_PROP_FRAME_WIDTH),
                 (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT));
-    //int codec = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));
-    //Size inputSize = Size(640, 480);
-    std::cout << inputSize;
+    std::cout << "Input Resolution: " << inputSize << std::endl;
+
     VideoWriter outputVideo;
-    //outputVideo.open("output.avi", -1, cap.get(CV_CAP_PROP_FPS), inputSize, true);
-    outputVideo.open("output.avi", CV_FOURCC('X', 'V', 'I', 'D'), 15, inputSize, true);
+    outputVideo.open("output.avi", CV_FOURCC('X', 'V', 'I', 'D'), fps, inputSize, true);
 
     if (!outputVideo.isOpened()) {
         std::cout << "Problem. Couldn't open video output.";
-        //return -1;
     }
 
     Mat image;
     bool x = true;
     int n = 0;
-    namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
     while (x && n < 100) {
-        bool x = cap.read(image);
+        cap >> image;
         outputVideo << image;
-        n++;
-        imshow("Display Image", image);
-        //waitKey(0);
+        ++n;
     }
     return 0;
 }
