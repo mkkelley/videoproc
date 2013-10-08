a2fa1a8bbba44b1c4651e5f060e5c0aca7ffd79e
Michael Kelley <mike@etszone.com>
2013-10-08 08:40:10 -0500

    Extracted showing help in main().

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,15 @@ using std::cout;
 using std::endl;
 using std::to_string;
 
+void showHelp() {
+    cout << "Valid options include:\n";
+    cout << "\tstitch\n";
+    cout << "\trecord\n";
+}
+
 int main(int argc, char **argv) {
     if (argc == 1) {
-        cout << "Valid options include:" << endl;
-        cout << argv[0] << " " << "stitch" << endl;
-        cout << argv[0] << " " << "record" << endl;
+        showHelp();
         return 1;
     }
     if (string(argv[1]) == "stitch") {
