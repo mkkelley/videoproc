6659b43b733d71dedd37f627869008a8c4254106
Michael Kelley <m.kelley.3154@gmail.com>
2013-12-20 11:24:24 -0600

    Fix test_ThreadPool.

--- test/parallel/test_ThreadPool.cpp
+++ test/parallel/test_ThreadPool.cpp
@@ -3,28 +3,35 @@
 #include <functional>
 #include <iostream>
 
+std::mutex _cout_lock;
+
 void test_func_c() {
     for (int i = 0; i < 10; i++) {
+        std::unique_lock<std::mutex> l(_cout_lock);
         std::cout << "hi" << std::endl;
     }
 }
 
 void test_func_g() {
     for (int i = 0; i < 10; i++) {
+        std::unique_lock<std::mutex> l(_cout_lock);
         std::cout << "low" << std::endl;
     }
 }
 
 int main() {
-    ThreadPool tp(4);
+    std::shared_ptr<ThreadPool> tp(new ThreadPool(4));
+    tp->start();
     std::function<void()> f(test_func_c);
 
-    tp.submit(test_func_g);
-    tp.submit(test_func_c);
+    tp->submit(test_func_g);
+    tp->submit(test_func_c);
 
     for (int i = 0; i < 10; i++) {
+        std::unique_lock<std::mutex> l(_cout_lock);
         std::cout << "test" << std::endl;
     }
-    tp.wait(0);
+    tp->wait(0);
+    tp->terminate_all_workers();
     return 0;
 }
